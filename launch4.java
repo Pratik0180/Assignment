2bfdce1abbc5984ea7168257f7d06f75a9aa1c9b
Pratik0180 <127649964+Pratik0180@users.noreply.github.com>
2023-04-07 13:00:02 +0530

    Add files via upload

---
+++ launch4.java
@@ -0,0 +1,28 @@
+package assignment1;
+
+public class launch4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n=10;
+		
+		for(int i=0; i<n; i++)
+		{
+			for (int j=0; j<n; j++)
+			{
+				if(i-j>=(n-1)/2 || i==n-1 || i+j>=n-1 + (n-1)/2 )
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ launch6.java
@@ -0,0 +1,87 @@
+package assignment1;
+
+public class launch6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=10;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(j==0 && i<=0 && i>=0 || j==0 && i>=n/4)
+                    System.out.print("*");
+
+                else
+
+                    System.out.print(" ");
+                System.out.print(" ");
+
+
+            }
+for(int j=0; j<n; j++)
+            {
+                if(j==0  i==j  j==n-1)
+
+                    System.out.print("");
+
+                else
+
+                    System.out.print(" ");
+                System.out.print(" ");
+            }
+            for(int j=0; j<n; j++)
+            {
+                if(j==0  i==0  i==(n-1)/2 || i==n-1)
+
+                    System.out.print("");
+
+                else
+
+                    System.out.print(" " + " "+ " ");
+                System.out.print(" ");
+
+
+            }
+            System.out.print("");
+            for(int j=0; j<n; j++)
+            {
+                if(j==0 && i<=n-1  j==n-1 && i<=n-1  i==n-1 && j>=0 && j<=n-1)
+
+                    System.out.print("");
+
+                else
+
+                    System.out.print(" ");
+                System.out.print(" ");
+            }
+            for(int j=0; j<n; j++)
+            {
+                if(j==0  i==0 && j<=(n-1)/2  i==(n-1)/2 && j<=(n-1)/2 
+                        j==(n-1)/2 && i<=(n-1)/2  i-j==(n-1)/2)
+
+                    System.out.print("");
+
+                else
+
+                    System.out.print(" ");
+                System.out.print(" ");
+            }
+            for(int j=0; j<n; j++)
+            {
+                if(i==0 && j>0 && j<n-1 
+                        i==n-1 && j>0 && j<n-1 
+                        j==0 && i>0 && i<n-1 ||
+                        j==n-1 && i>0 && i<n-1)
+
+                    System.out.print("*");
+
+                else
+
+                    System.out.print(" ");
+                System.out.print(" ");
+            }
+
+	}
+
+}
